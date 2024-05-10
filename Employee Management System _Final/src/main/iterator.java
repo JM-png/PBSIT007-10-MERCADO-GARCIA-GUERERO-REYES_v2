faf20a2073f43f412c1f1f978f3fde571d591cd0
JM-png <68585526+JM-png@users.noreply.github.com>
2024-05-10 13:03:35 +0800

    FINAL
    Sir eto po ung pinaka updated na file namin

---
+++ Employee Management System _Final/src/main/iterator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author acer
+ */
+class iterator {
+
+    static String next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static boolean hasNext() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
